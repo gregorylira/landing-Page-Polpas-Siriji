553950847e028cfc76f68d404c25fc94db96b432
gregorylira <gregory.lira007@gmail.com>
2022-03-05 12:47:47 -0300

    💅 style: favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Home | ig.news </title>
+        <title>Home | Polpas Siriji </title>
       </Head>
 
       <main className={styles.contentContainer} id="inicio">
